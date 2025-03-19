b4bb42f536b864f9cb4aa86a68d1fc61b8bdc1e9
Keerthimani020 <2322jb25@kgcas.com>
2025-03-19 15:25:28 +0530

    Add files via upload

---
+++ StringMethods.java
@@ -0,0 +1,11 @@
+package session3;
+
+public class StringMethods {
+	
+	public static void main(String[] args) {
+		String s1 = "Java Program";
+		//1.length
+		System.out.println(s1.length());
+	}
+
+}
