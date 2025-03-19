9d75ada744746c4ebabd8b0193585c5829e9baba
Keerthimani020 <2322jb25@kgcas.com>
2025-03-19 15:18:50 +0530

    Add files via upload

---
+++ StringsEx.java
@@ -0,0 +1,17 @@
+package session3;
+
+public class StringsEx {
+	
+	public static void main(String[] args) {
+		
+		String s1 = "Mugilan"; //String literal
+		String s2 = new String("Mosses sir"); //String Object
+		char[] c1 = {'J', 'A', 'V', 'A'}; //Array of characters
+		String s3 = new String(c1); //char to string
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+	}
+
+}
